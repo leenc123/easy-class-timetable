2af14db0bfec5e2a0ff4f2cbf84d4f33b59f6321
李恩超 <470360046@qq.com>
2026-04-09 16:46:16 +0800

    优化获取教师和学生列表的API请求，添加分页参数

--- frontend/src/views/Courses.vue
+++ frontend/src/views/Courses.vue
@@ -223,8 +223,8 @@ const fetchData = async () => {
 
 const fetchOptions = async () => {
   const [teachers, students] = await Promise.all([
-    teacherApi.getList({ page_size: 1000 }),
-    studentApi.getList({ page_size: 1000 })
+    teacherApi.getList({ page: 1, page_size: 1000 }),
+    studentApi.getList({ page: 1, page_size: 1000 })
   ])
   teacherOptions.value = teachers.data || []
   studentOptions.value = students.data || []
